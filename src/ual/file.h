976e64c2065e29a4094b38683252c727f6e0d980
tiannian <dtiannian@aliyun.com>
2019-12-04 15:08:39 +0800

    modify by dumeng

--- src/ual/file.h
+++ src/ual/file.h
@@ -18,46 +18,51 @@ public:
     typedef int offset_t;
     typedef int mode_t;
     
-    enum class errcode {
+    enum class flag_t {
+        RD,
+        WR,
+        RDWD,
+    };
+    
+    enum class errcode_t {
         success = 0,
         open_faild = 101,
         write_faild,
         read_faild
     };
     /*
-    * open file start 
-    */
-    void open(const string &_path, flag_t flag, mode_t mode, function<void(errcode _errcode)> callback)
+     * @brief open file start.
+     *
+     * xxxx
+     * 
+     * @params 
+     * @return
+     * 
+     */
+    void open(const string &_path, flag_t flag, function<void(errcode_t _errcode)> callback)
     {
         sub_this->on_open(_path,flag,callback);
     }
+    
     /*
-    * only read file 
-    */
-    void read(size_t length, offset_t offset, function<void(errcode _errcode, string _data)> callback)
+     * @brief read file.
+     */
+    void read(size_t length, offset_t offset, function<void(errcode_t _errcode, string _data)> callback)
     {
         sub_this->on_read(length,offset,callback);
     }
 
     /*
-    *only write file
-    */
-    void write(const string &data, offset_t offset, function<void(errcode _errcode)> callback)
+     * write file
+     */
+    void write(const string &data, offset_t offset, function<void(errcode_t _errcode)> callback)
     {
         sub_this->on_write(data,offset,callback);
     }
 
-   /*
-   *init need buffer for read or write
-   */
-    /*void init(char *pbuf)
-    {
-        _pbuf=pbuf;
-        sub_this->buf_init(_pbuf);
-    }*/
-   /*
-   *close file and save
-   */
+    /*
+     * @brief close file and save
+     */
     int close()
     {
         return sub_this->on_close();
--- src/ual/impls/impl_file.h
+++ src/ual/impls/impl_file.h
@@ -14,76 +14,79 @@ using namespace std;
 
 namespace ual{
 
-static void file_cb(uv_fs_t *cb);
-class libuv_file:public file<libuv_file>{
+class libuv_file: public file<libuv_file>{
 public:
-    using file<libuv_file>::file;
     /*
-    *use executor to construct libuv_file
-    */
+     * use executor to construct libuv_file
+     */
     template<class ExecutorImpl>
-    libuv_file(const executor<ExecutorImpl> &exec){
-        this->f_init(exec);
-        this->callback = [](){};
-    }
-    ~libuv_file()
-    {
+    libuv_file(const executor<ExecutorImpl> &exec):
+        _loop(*(static_cast<const ExecutorImpl*>(&exec) -> loop)，
+        _buf(nullptr) {}
+
+    ~libuv_file() {
         uv_fs_req_cleanup(&_file);
     }
-    template<class ExecutorImpl>
-    void f_init(const executor<ExecutorImpl> &exec);
-    void on_open(const string &_path, flag_t flag, mode_t mode, function<void(errcode _errcode)> _callback);
-    void on_read(size_t length, offset_t offset, function<void(errcode _errcode, string _data)> _callback);
-    void on_write(const string &data, offset_t offset, function<void(errcode _errcode)> _callback);
+
+    void on_open(const string &_path, flag_t flag, function<void(errcode_t _errcode)> _callback);
+    
+    void on_read(size_t length, offset_t offset, function<void(errcode_t _errcode, string _data)> _callback);
+    
+    void on_write(const string &data, offset_t offset, function<void(errcode_t _errcode)> _callback);
+    
     int on_close();
 
     friend file<libuv_file>;
+
 public:
-    function<void(void)> callback;
-    errcode _errcode;
+    function<void(errcode_t _errcode)> open_callback;
+    function<void(errcode_t _errcode, string _data)> read_callback;
+    function<void(errcode_t _errcode)> write_callback;
+
 private:
     uv_fs_t _file;
-    uv_fs_t _read_file;
-    uv_fs_t _write_file;
-    uv_loop_t *f_loop;
+    uv_loop_t *_loop;
+    string _result;
+    uv_buf_t *_buf
 };
 
-static void file_cb(uv_fs_t *cb)
-{
+static void on_file_open(uv_fs_t *_fs) {
     auto data = uv_handle_get_data(reinterpret_cast<uv_handle_t*>(cb));
     auto file = static_cast<libuv_file *>(data);
-    file->callback();
-//    file->_file =cb;
+    if (file->result < 0) {
+        this->open_callback(file::errcode_t::open_faild);
+    } else {
+        this->open_callback(file::errcode_t::success);
+    }
 }
 
-template<class ExecutorImpl>  
-void libuv_file::f_init(const executor<ExecutorImpl> &exec)
-{
-    auto *impl_exec = static_cast<const ExecutorImpl*>(&exec);
-    this->f_loop = const_cast<uv_loop_t*>(&impl_exec->loop);
-    //uv_handle_set_data(reinterpret_cast<uv_handle_t*>(&(this->_file)),this);
-}
-
-void libuv_file::on_open(const string &_path, flag_t flag, mode_t mode,function<void(errcode _errcode)> _callback)
+void libuv_file::on_open(const string &_path, flag_t flag,function<void(errcode _errcode)> _callback)
 {   
     uv_handle_set_data(reinterpret_cast<uv_handle_t*>(&(this->_file)),this);
-    if(uv_fs_open(this->f_loop,&(this->_file),_path.c_str(),flag,mode,file_cb))
-    {
-        this->_errcode = errcode::success;
+    
+    auto result = uv_fs_open(this->f_loop,&(this->_file),_path.c_str(),flag,xxxxx,on_file_open);
+    if (result != 0) {
+        _callback(file::errcode_t::open_faild);
     }
-    else
-    {
-       this-> _errcode = errcode::open_faild;
-    }
-    _callback(this->_errcode);
+    
+    this->open_callback = _callback;
+}
+
+static void on_file_read(uv_fs_t *_fs) {
     
 }
+
 void libuv_file::on_read(size_t length, offset_t offset, function<void(errcode _errcode, string _data)> _callback)
 {
     uv_handle_set_data(reinterpret_cast<uv_handle_t*>(&(this->_file)),this);
-    char rbuf[length+1]={0};
-    uv_buf_t _rbuf_t = uv_buf_init(rbuf,length+1);
-    if(uv_fs_read(this->f_loop,&(this->_file),this->_file.result,&_rbuf_t,1,offset,file_cb))
+    
+    // qing kon
+    this->_result.clear();
+    this->_result.resize(length);
+    
+    this->_buf = uv_buf_init(this->_result.data(),length+1);
+
+    auto result = uv_fs_read(this->f_loop,&(this->_file),this->_file.result,&_buf,1,offset,on_file_read)
     {
         this->_errcode = errcode::success;
     }
